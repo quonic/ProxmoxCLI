6b5d55e46b01eac5d204a5b2554de8fe5655b620
Spyingwind <spyingwind@gmail.com>
2017-08-18 15:17:24 -0500

    Merged Qemu class into Node.ps1

--- ProxmoxCLI/classes/Node.ps1
+++ ProxmoxCLI/classes/Node.ps1
@@ -159,3 +159,25 @@ class Node {
         return $false
     }
 }
+
+class Qemu {
+    
+    [string] $vmid
+    [PSCustomObject] $AvailableResources
+    [PSCustomObject] $Status
+    [Node] $Node
+        
+        
+    Qemu ([Node] $Node, [string] $vmid) {
+        $this.vmid = $vmid
+        $this.Node = $Node
+        $this.AvailableResources = (callREST -Resource "nodes/$($this.Node.Name)/qemu/$($this.vmid)")
+        $this.Status = $this.getStatus()
+    }
+        
+    [PSCustomObject] getStatus() {
+        return (callREST -Resource "nodes/$($this.Node.Name)/qemu/$($this.vmid)/status/current")
+        
+    }
+
+}
