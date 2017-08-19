d622356e18e9c507d816726f7fa6cd3f1e6bb98a
Spyingwind <spyingwind@gmail.com>
2017-08-19 11:01:53 -0500

    Updated Help docs for functions Get-Node and Get-Qemu.

--- ProxmoxCLI/public/Nodes.ps1
+++ ProxmoxCLI/public/Nodes.ps1
@@ -1,15 +1,36 @@
+<#
+.SYNOPSIS
+Retruns Nodes from currently connected Proxmox host
+
+.DESCRIPTION
+Retruns Nodes from currently connected Proxmox host or can return a specific Node.
+
+.PARAMETER node
+Node name
+
+.EXAMPLE
+Get-Node
+
+.EXAMPLE
+Get-Node -Node "pvehost1"
+
+.EXAMPLE
+$pvehost1 = Get-Node -Node "pvehost1"
+
+.NOTES
+The object(s) returned can be used to manipulate node(s)
+#>
 function Get-Node {
     [CmdletBinding()]
     Param(
-        # Parameter help description
         [Parameter(ValueFromPipelineByPropertyName)]
         [String]
-        $node
+        $Node
     )
 
     # TODO - Expand this to return more information, probably in Node class.
-    if ($node) {
-        return $node | ForEach-Object {
+    if ($Node) {
+        return $Node | ForEach-Object {
             New-Object -TypeName "Node" -ArgumentList $_
         }
     }
@@ -20,14 +41,34 @@ function Get-Node {
     }
 }
 
+<#
+.SYNOPSIS
+Retruns a Qemu from the specified Node
+
+.DESCRIPTION
+Retruns a Qemu from the specified Node
+
+.PARAMETER vmid
+The Qemu property called vmid
+
+.PARAMETER Node
+Name of node that the Qemu is running under
+
+.EXAMPLE
+Get-Qemu -vmid "101" -Node "pvehost1"
+
+.EXAMPLE
+$vm101 = Get-Qemu -vmid "101" -Node "pvehost1"
+
+.NOTES
+The object(s) returned can be used to manipulate qemu(s)
+#>
 function Get-Qemu {
     [CmdletBinding()]
     Param(
-        # The Qemu property called vmid
         [Parameter(mandatory = $true, ValueFromPipelineByPropertyName)]
         [String]
         $vmid,
-        # Name of node that the Qemu is running under
         [Parameter(mandatory = $true)]
         [String]
         $Node
