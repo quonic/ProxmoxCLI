1714f95419dd72f4fa4257aca39f4b1f7d6ec5d0
JCaten <jimcaten@gmail.com>
2020-02-25 14:22:10 -0600

    Refactors Get-Qemu into Get-Guest Updates Get-Node and Get-Guest help comments

--- ProxmoxCLI/public/Nodes.ps1
+++ ProxmoxCLI/public/Nodes.ps1
@@ -1,26 +1,26 @@
-<#
-.SYNOPSIS
-Retruns Nodes from currently connected Proxmox host
-
-.DESCRIPTION
-Retruns Nodes from currently connected Proxmox host or can return a specific Node.
-
-.PARAMETER node
-Node name
-
-.EXAMPLE
-Get-Node
-
-.EXAMPLE
-Get-Node -Node "pvehost1"
-
-.EXAMPLE
-$pvehost1 = Get-Node -Node "pvehost1"
-
-.NOTES
-The object(s) returned can be used to manipulate node(s)
-#>
 function Get-Node {
+    <#
+    .SYNOPSIS
+    Retruns Nodes from currently connected Proxmox host
+
+    .DESCRIPTION
+    Retruns Nodes from currently connected Proxmox host or can return a specific Node.
+
+    .PARAMETER node
+    Node name
+
+    .EXAMPLE
+    Get-Node
+
+    .EXAMPLE
+    Get-Node -Node "pvehost1"
+
+    .EXAMPLE
+    $pvehost1 = Get-Node -Node "pvehost1"
+
+    .NOTES
+    The object(s) returned can be used to manipulate node(s)
+    #>
     [CmdletBinding()]
     Param(
         [Parameter(ValueFromPipelineByPropertyName)]
@@ -43,51 +43,91 @@ function Get-Node {
     }
 }
 
-<#
-.SYNOPSIS
-Retruns a Qemu from the specified Node
+function Get-Guest {
+    <#
+    .SYNOPSIS
+    Retruns a Guest from the specified Node
 
-.DESCRIPTION
-Retruns a Qemu from the specified Node
+    .DESCRIPTION
+    Retruns a Guest from the specified Node
 
-.PARAMETER vmid
-The Qemu property called vmid
+    .PARAMETER vmid
+    The Guest property called vmid
 
-.PARAMETER Node
-Name of node that the Qemu is running under
+    .PARAMETER Node
+    Name of node that the Guest is running under
 
-.EXAMPLE
-Get-Qemu -vmid "101" -Node "pvehost1"
+    .EXAMPLE
+    Get-Guest -vmid "101" -Node "pvehost1"
 
-.EXAMPLE
-$vm101 = Get-Qemu -vmid "101" -Node "pvehost1"
+    .EXAMPLE
+    $vm101 = Get-Guest -vmid "101" -Node "pvehost1"
 
-.NOTES
-The object(s) returned can be used to manipulate qemu(s)
-#>
-function Get-Qemu {
+    .NOTES
+    The object(s) returned can be used to manipulate Guest(s)
+    #>
     [CmdletBinding()]
     Param(
         [Parameter(mandatory = $false, ValueFromPipelineByPropertyName)]
         [String]
-        $vmid,
+        $Id,
         [Parameter(mandatory = $true)]
         [String]
-        $Node
+        $Node,
+        [Parameter(Mandatory = $false, ParameterSetName = "OnlySpecificType")]
+        [Parameter(mandatory = $false)]
+        [switch]
+        $ContainerOnly,
+        [Parameter(Mandatory = $false, ParameterSetName = "OnlySpecificType")]
+        [Parameter(mandatory = $false)]
+        [switch]
+        $VmOnly
     )
-    if ($vmid) {
-        return [Qemu]::new($Node, $vmid)
+    $vms = Invoke-ProxmoxAPI -Resource "nodes/$($Node)/qemu"
+    $containers = Invoke-ProxmoxAPI -Resource "nodes/$($Node)/lxc"
+    if ($Id) {
+        if (($vms | Where-Object { $_.vmid -eq $Id }).Count -eq 1) {
+            return [PSCustomObject]@{
+                id   = $vms.vmid
+                node = $Node
+            }
+        }
+        elseif (($containers | Where-Object { $_.vmid -eq $Id }).Count -eq 1) {
+            return [PSCustomObject]@{
+                id   = $containers.vmid
+                node = $Node
+            }
+        }
+        else {
+            throw "No VM or Container, or more than one guest exists with the ID of $Id"
+        }
     }
     else {
-        return Get-Node -Node $Node | ForEach-Object {
-            $VMList = $_.getQemu()
-            $VMList | ForEach-Object {
-                # Return Qemu object
-                $_
+        if ($ContainerOnly) {
+            return $containers | ForEach-Object {
+                [PSCustomObject]@{
+                    id   = $_.vmid
+                    node = $Node
+                }
+            }
+        }
+        elseif ($VmOnly) {
+            $vms | ForEach-Object {
+                [PSCustomObject]@{
+                    id   = $_.vmid
+                    node = $Node
+                }
+            }
+        }
+        else {
+            return $vms + $containers | ForEach-Object {
+                [PSCustomObject]@{
+                    id   = $_.vmid
+                    node = $Node
+                }
             }
         }
     }
-
 }
 
-Export-ModuleMember -Function @('Get-Node', 'Get-Qemu')
+Export-ModuleMember -Function @('Get-Node', 'Get-Guest')
