e4e14cd3a59b9f78e1da618ece4cccdc395f53f9
quonic <jimcaten@gmail.com>
2020-02-27 13:36:29 -0600

    Adds Get-Syslog, might need to parse the logs

--- ProxmoxCLI/public/Nodes.ps1
+++ ProxmoxCLI/public/Nodes.ps1
@@ -105,4 +105,79 @@ function Get-Node {
     return $Nodes
 }
 
-Export-ModuleMember -Function @('Get-Node')
+function Get-Syslog {
+    <#
+    .SYNOPSIS
+    Reads the system log
+
+    .DESCRIPTION
+    Reads the system log
+
+    .PARAMETER Node
+    Node to get the logs from
+
+    .PARAMETER Limit
+    Number of lines to show
+
+    .PARAMETER Service
+    Service ID
+
+    .PARAMETER Since
+    Start date-time to start from
+
+    .PARAMETER Start
+    Line number to start at
+
+    .PARAMETER Until
+    Ends date-time to end at
+
+    .EXAMPLE
+    Get-Node | Get-Syslog
+
+    .NOTES
+    General notes
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [String]
+        $Node,
+        [int]
+        $Limit,
+        # Service ID
+        [string]
+        $Service,
+        # Display all log since this date-time string
+        [ValidatePattern("^\d{4}-\d{2}-\d{2}( \d{2}:\d{2}(:\d{2})?)?$")]
+        [string]
+        $Since,
+        [int]
+        $Start,
+        # Display all log until this date-time string
+        [ValidatePattern("^\d{4}-\d{2}-\d{2}( \d{2}:\d{2}(:\d{2})?)?$")]
+        [string]
+        $Until
+    )
+    $Options = @()
+    if ($Limit) {
+        $Options.Add("limit" , $Limit)
+    }
+    if ($Service) {
+        $Options.Add("service" , $Service)
+    }
+    if ($Since) {
+        $Options.Add("since" , $Since)
+    }
+    if ($Start) {
+        $Options.Add("start" , $Start)
+    }
+    if ($Until) {
+        $Options.Add("until" , $Until)
+    }
+    # TODO parse the lines?
+    return (Invoke-ProxmoxAPI -Method Post -Resource "nodes/$($Node)/qemu/$($Id)/status/reboot" -Options $Options)
+}
+
+
+Export-ModuleMember -Function @('Get-Node', 'Get-Syslog')
