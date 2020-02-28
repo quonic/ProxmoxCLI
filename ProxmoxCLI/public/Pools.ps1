c654bba982b95298cd245b21e396d736dd0d0e77
quonic <jimcaten@gmail.com>
2020-02-28 13:23:12 -0600

    Adds help info to Get-Pool

--- ProxmoxCLI/public/Pools.ps1
+++ ProxmoxCLI/public/Pools.ps1
@@ -1,4 +1,23 @@
 function Get-Pool {
+    <#
+    .SYNOPSIS
+    Returns the pool(s) configuration
+
+    .DESCRIPTION
+    Returns either a pool or multiple pools configuration
+
+    .PARAMETER PoolId
+    The Pool Id
+
+    .EXAMPLE
+    Get-Pool
+
+    .EXAMPLE
+    Get-Pool -PoolId "Pool1"
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName, ValueFromPipeline)]
